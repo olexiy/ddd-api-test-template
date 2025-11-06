a453dcf43672e51b17c948e09204078642875038
Olexiy Sokurenko <ol.sakura@gmail.com>
2025-11-06 22:24:05 +0100

    Initial commit: Spring Boot project setup

---
+++ src/test/java/de/olexiy/ddd_api_test_template/DddApiTestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package de.olexiy.ddd_api_test_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddApiTestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
